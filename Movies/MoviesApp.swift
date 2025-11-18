00a8845bc5fe37f0add691ae4528418c5ab66b07
RemiTyberghien <remi.tyberghien@student.vives.be>
2025-11-18 09:45:14 +0100

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Remi.Tyberghien on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
